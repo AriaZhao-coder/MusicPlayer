848217067957dafa6274766987d97bf06b411855
AriaZhao-coder <yizhao1027@163.com>
2025-02-20 21:38:28 +0800

    Songs02

--- src/app/(tabs)/(songs)/index.tsx
+++ src/app/(tabs)/(songs)/index.tsx
@@ -2,21 +2,31 @@ import { TrackList } from "@/app/components/TracksList";
 import { screenPadding } from "@/constants/tokens";
 import { useNavigationSearch } from "@/hooks/useNavigationSearch";
 import { defaultStyles } from "@/styles";
+import { useMemo } from "react";
 import { ScrollView, Text, View } from 'react-native'
+import library from "@/assets/data/library.json"
+import { trackTitleFilter } from "@/helpers/filter";
+
 
 const SongsScreen = () =>{
     const search = useNavigationSearch({
         searchBarOptions: {
             placeholder: 'Find in songs',
-        }
+        },
     })
+
+    const filteredTracks = useMemo(() => {
+        if (!search) return library
+
+        return library.filter(trackTitleFilter(search))
+    },[search])
     return( 
         <View style={defaultStyles.container}>
             <ScrollView
                 contentInsetAdjustmentBehavior="automatic"
                 style={{ paddingHorizontal: screenPadding.horizontal}}
             >
-                <TrackList scrollEnabled={false}/>
+                <TrackList tracks={filteredTracks} scrollEnabled={false}/>
             </ScrollView>
         </View>
     )
